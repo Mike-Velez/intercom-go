d5c5e0117aaff871e38e15f0a3dccdb6751aa148
Mike Velez <mike@spotpet.com>
2024-06-24 12:44:20 -0500

    updates

--- intercom.go
+++ intercom.go
@@ -1,7 +1,7 @@
 package intercom
 
 import (
-	"gopkg.in/intercom/intercom-go.v2/interfaces"
+	"gopkg.in/Mike-Velez/intercom-go.v2/interfaces"
 )
 
 // A Client manages interacting with the Intercom API.
--- message_api.go
+++ message_api.go
@@ -3,7 +3,7 @@ package intercom
 import (
 	"encoding/json"
 
-	"gopkg.in/intercom/intercom-go.v2/interfaces"
+	"gopkg.in/Mike-Velez/intercom-go.v2/interfaces"
 )
 
 // MessageRepository defines the interface for creating and updating Messages through the API.
